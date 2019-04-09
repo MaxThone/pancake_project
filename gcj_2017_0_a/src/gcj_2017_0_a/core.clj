f3a8f57df511d50755e8113fc9290726bd2399c1
Max Thone <max.thone@aimia.com>
2019-04-09 12:01:27 +0100

    :w

--- gcj_2017_0_a/src/gcj_2017_0_a/core.clj
+++ gcj_2017_0_a/src/gcj_2017_0_a/core.clj
@@ -1,6 +1,83 @@
 (ns gcj-2017-0-a.core)
 
-(defn foo
-  "I don't do a whole lot."
+(require '[clojure.string :as str])
+
+; CORE FUNCTIONS
+(defn replace_array
+  [p_array flip_array idx]
+  (if (= (count flip_array) 0)
+    ;; Flip array is empty, so no need to do anything:
+    p_array
+    ;; Flip array contains elements we want to sub:
+    (replace_array (assoc p_array idx (get flip_array 0))
+                   (subvec flip_array 1)
+                   (+ idx 1))))
+; Idee: Hou een soort 'flip mode bij'.
+(defn flip
   [x]
-  (println x "Hello, World!"))
+  (if (= \- x) \+ \-))
+
+(defn flip-array
+  [P F idx]
+  (vec (map flip (subvec P idx (+ idx F)))))
+
+; zet index in the recursion: Je gaat nu voor elke pannekoek de hele array door
+(defn new_flipping_pancakes
+  [P F flips]
+  (let [idx (.indexOf P \-)]
+    (if (= idx -1)
+      (str flips)
+      (if (> (+ idx F) (count P))
+        (str "IMPOSSIBLE")
+        (new_flipping_pancakes (replace_array P (flip-array P F idx) idx) F (inc flips))))))
+; FUNCTIONS HELPING WITH READING/WRITING ETC.
+
+; Read files:
+(defn read_per_line [file_path]
+  (with-open [rdr (clojure.java.io/reader file_path)]
+    (reduce conj [] (line-seq rdr))))
+
+; Write files
+(defn write_lines [filename data]
+  (with-open [wrt (clojure.java.io/writer filename)]
+    (doseq [x data]
+      (.write wrt (str x "\n")))))
+
+(defn solver
+  [vector_unit]
+  (let [P (vec (first vector_unit))
+        F (Integer. (second vector_unit))
+        ] (new_flipping_pancakes P F 0)))
+
+; TEST CASES
+(defn sample
+  []
+  (println (new_flipping_pancakes "-+--" 1 0))
+  (println (new_flipping_pancakes "-+--" 2 0))
+  (println (new_flipping_pancakes "-+--" 1 0))
+  (println (new_flipping_pancakes "-+--" 1 0))
+  (println (new_flipping_pancakes "-+--" 1 0))
+  (println (new_flipping_pancakes "-+--" 3 0))
+  (println (new_flipping_pancakes "-+--" 4 0))
+  (println (new_flipping_pancakes "-+--" 1 0))
+  (println (new_flipping_pancakes "-+--+--++-----+" 3 0))
+  (println (new_flipping_pancakes "-+--+++---+++" 3 0))
+  (println (new_flipping_pancakes "-+--" 2 0))
+  (println (new_flipping_pancakes "-+--+--+" 1 0))
+  (println (new_flipping_pancakes "-+--+--+" 3 0))
+  (println (new_flipping_pancakes "-+--++-+" 3 0))
+  )
+; READ FROM FILE, WRITE SOLUTION TO FILE
+(def input_vector
+  (let [split_by_space (fn [x] (str/split x #" "))]
+    (map split_by_space (subvec (read_per_line "input/A-small-practice.in") 1))))
+
+(def output_vector (map solver input_vector))
+
+(def output_data (map str
+                      (repeat (count input_vector) "case #")
+                      (range 1 ( + (count input_vector) 1))
+                      (repeat (count input_vector) ": ")
+                      output_vector))
+
+(write_lines "data/output/large_output.txt" output_data)
